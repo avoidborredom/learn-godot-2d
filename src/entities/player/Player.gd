dd0a562d465d3e1d731e13fd2f18cae76feb98df
avoidborredom <davidpborromeo@gmail.com>
2023-09-30 20:03:05 +0900

    feat: add death animation

--- src/entities/player/Player.gd
+++ src/entities/player/Player.gd
@@ -14,47 +14,56 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("mouse_click"):
-		target = get_global_mouse_position()
-		velocity = position.direction_to(target) * speed
+	if $AnimatedSprite2D.animation != "death":
+		var velocity = Vector2.ZERO # The player's movement vector.
+		
+		if Input.is_action_pressed("right"):
+			velocity.x += 1
+		if Input.is_action_pressed("left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("down"):
+			velocity.y += 1
+		if Input.is_action_pressed("up"):
+			velocity.y -= 1
+		if Input.is_action_pressed("mouse_click"):
+			target = get_global_mouse_position()
+			velocity = position.direction_to(target) * speed
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
-	
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
-	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		# See the note below about boolean assignment.
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "down"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+		
+		
+		
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * speed
+			$AnimatedSprite2D.play()
+		else:
+			$AnimatedSprite2D.stop()
+		
+		position += velocity * delta
+		position = position.clamp(Vector2.ZERO, screen_size)
+		
+		if velocity.x != 0:
+			$AnimatedSprite2D.animation = "walk"
+			$AnimatedSprite2D.flip_v = false
+			# See the note below about boolean assignment.
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		elif velocity.y != 0:
+			$AnimatedSprite2D.animation = "down"
+			$AnimatedSprite2D.flip_v = velocity.y > 0
 
 
 func _on_body_entered(body):
+	$AnimatedSprite2D.play("death")
+	await $AnimatedSprite2D.animation_finished
 	hide() # Player disappears after being hit.
+	$CollisionShape2D.set_deferred("disabled", true)
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
 
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+	$AnimatedSprite2D.animation = "walk"
 	
+
+
