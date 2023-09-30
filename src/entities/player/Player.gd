48e33181513789e94ebab07d04d9c6d5d383376c
avoidborredom <davidpborromeo@gmail.com>
2023-09-30 16:15:56 +0900

    feat: modify text and minor stylings

--- src/entities/player/Player.gd
+++ src/entities/player/Player.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 signal hit
 
-@export var speed = 100 # How fast the player will move (pixels/sec).
+@export var speed = 200 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 var target = position
 
